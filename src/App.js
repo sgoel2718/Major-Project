36c8d568da3c6c7b2356841a565354a28e185ed0
Aditya Bikram Arandhara <adityabikrambill@gmail.com>
2022-07-07 17:22:06 +0530

    Added my own comment

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="App">
       {/* <header> */}
+      {/* Aditya */}
       <ResponsiveAppBar />
       <Routes>
         <Route path="/" element={<Opportunities />} />
