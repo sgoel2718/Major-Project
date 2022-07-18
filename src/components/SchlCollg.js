1199ac9e2ab4441690368b4e1a21af006bf92a01
Shubham <sgoel2718@gmail.com>
2022-07-19 00:18:44 +0530

    School and College Section Update

--- src/components/SchlCollg.js
+++ src/components/SchlCollg.js
@@ -4,71 +4,82 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import "./SchlCollg.css";
-
-const Item = styled(Paper)(({ theme }) => ({
-	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-	...theme.typography.body2,
-	padding: theme.spacing(1),
-	textAlign: "center",
-	color: theme.palette.text.secondary,
-}));
+import sc1 from "./images/sc1.jpg";
+import sc2 from "./images/sc2.jpg";
+import sc3 from "./images/sc3.jfif";
+import sc4 from "./images/sc4.jpg";
 
 export default function AutoGrid() {
 	return (
-		<Box
-			sx={{
-				marginLeft: "25%",
-				marginRight: "25%",
-				marginTop: "4%",
-				marginBottom: "4%",
-			}}
-		>
-			<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-				<Grid item lg={6} xs={12}>
-					<div
-						// className="col-lg-3 col-sm-6 wow fadeInUp"
-						data-wow-delay="0.1s"
-						style={{
-							visibility: "visible",
-							animationDelay: "0.1s",
-							animationName: "fadeInUp",
-						}}
-					>
-						<div className="service-item text-center pt-3">
-							<div className="p-4">
-								<i className="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
-								<h5 className="mb-3">Skilled Instructors</h5>
-								<p>
-									Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita
-									amet diam
-								</p>
+		<>
+			<Box
+				sx={{
+					marginLeft: "5%",
+					marginRight: "5%",
+					marginTop: "4%",
+					marginBottom: "4%",
+				}}
+			>
+				<h4>Categories</h4>
+				<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+					<Grid item lg={6} xs={6}>
+						<Grid item lg={6} xs={6}>
+							<div
+								// className="col-lg-3 col-sm-6 wow fadeInUp"
+								data-wow-delay="0.1s"
+								style={{
+									visibility: "visible",
+									animationDelay: "0.1s",
+									animationName: "fadeInUp",
+								}}
+							>
+								<div className="">
+									<div className="">
+										<i className="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
+										<img src={sc1} style={{ width: "200%", height: "250px" }} />
+									</div>
+								</div>
+							</div>
+						</Grid>
+						<Grid item lg={6} xs={6} rowSpacing={2}>
+							<div
+								// className="col-lg-3 col-sm-6 wow fadeInUp"
+								data-wow-delay="0.1s"
+								style={{
+									visibility: "visible",
+									animationDelay: "0.1s",
+									animationName: "fadeInUp",
+								}}
+							>
+								<div className="service-item">
+									<div className="">
+										<i className="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
+										<img src={sc2} />
+									</div>
+								</div>
+							</div>
+						</Grid>
+					</Grid>
+					<Grid item lg={6} xs={6}>
+						<div
+							// className="col-lg-3 col-sm-6 wow fadeInUp"
+							data-wow-delay="0.1s"
+							style={{
+								visibility: "visible",
+								animationDelay: "0.1s",
+								animationName: "fadeInUp",
+							}}
+						>
+							<div className="service-item">
+								<div className="">
+									<i className="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
+									<img src={sc3} style={{ height: "425px" }} />
+								</div>
 							</div>
 						</div>
-					</div>
+					</Grid>
 				</Grid>
-				<Grid item lg={6} xs={12}>
-					<div
-						// className="col-lg-3 col-sm-6 wow fadeInUp"
-						data-wow-delay="0.1s"
-						style={{
-							visibility: "visible",
-							animationDelay: "0.1s",
-							animationName: "fadeInUp",
-						}}
-					>
-						<div className="service-item text-center pt-3">
-							<div className="p-4">
-								<i className="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
-								<h5 className="mb-3">Skilled Instructors</h5>
-								<p>
-									Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita
-									amet diam
-								</p>
-							</div>
-						</div>
-					</div>
-				</Grid>
-			</Grid>
-		</Box>
+			</Box>
+		</>
 	);
 }
