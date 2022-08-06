9b18d4801f30cacbfce2e85182ad5c3d9de0b718
Shubham <sgoel2718@gmail.com>
2022-08-06 13:23:57 +0530

    Education Updates

---
+++ src/components/Filter.js
@@ -0,0 +1,53 @@
+Name	Status	First Submitted	Last Activity	Score	Actions
+John's Steaks	Submitted	-	6 hours ago	88	...
+Bob's Burgers	Not Submitted	-	a day ago	69	...
+Dave's Bagels	Submitted	-	2 days ago	93	...
+Filters
+Status
+Submitted
+Invited
+Not Submitted
+Draft
+Information Request
+Loan Application
+First Submitted
+
+0100
+0
+100
+Last Activity
+
+0100
+0Name	Status	First Submitted	Last Activity	Score	Actions
+John's Steaks	Submitted	-	6 hours ago	88	...
+Bob's Burgers	Not Submitted	-	a day ago	69	...
+Dave's Bagels	Submitted	-	2 days ago	93	...
+Filters
+Status
+Submitted
+Invited
+Not Submitted
+Draft
+Information Request
+Loan Application
+First Submitted
+
+0100
+0
+100
+Last Activity
+
+0100
+0
+100
+MARQ™ score
+
+0100
+0
+100
+100
+MARQ™ score
+
+0100
+0
+100
--- src/components/School.js
+++ src/components/School.js
@@ -1,5 +1,87 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { Paper, Stack } from "@mui/material";
+import about from "./images/about.jpg";
+import { Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import TextD from "./TextD";
+import "./School.css";
 
 export default function School() {
-	return <div>School</div>;
+	return (
+		<>
+			<h3 className="text">
+				Schools <span className="highlight"> at your fingertips</span>
+			</h3>
+			<Box
+				sx={{
+					width: "100%",
+					marginTop: "15%",
+					paddingLeft: "5%",
+					marginBottom: "5%",
+				}}
+			>
+				<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+					<Grid item lg={9} xs={11}>
+						<Card>
+							<Card.Body>
+								<Card.Title>Special title treatment</Card.Title>
+								<Card.Text>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>Special title treatment</Card.Title>
+								<Card.Text>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>Special title treatment</Card.Title>
+								<Card.Text>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>Special title treatment</Card.Title>
+								<Card.Text>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>Special title treatment</Card.Title>
+								<Card.Text>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					</Grid>
+					<Grid item lg={3}>
+						<div className="alignment">
+							<TextD />
+						</div>
+					</Grid>
+				</Grid>
+			</Box>
+		</>
+	);
 }
